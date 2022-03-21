3fecca316a0720456498e07ccf54ca67f3a6f65c
Eleott-hi <laptev18vv@gmail.com>
2022-03-21 16:47:51 +0700

    main - OK

--- main.c
+++ main.c
@@ -3,29 +3,30 @@
 
 #include "extclib/http.h"
 
-void index_page(int conn, HTTPreq* request);
-void about_page(int conn, HTTPreq* request);
+void index_page(int conn, HTTPreq *req);
+void about_page(int conn, HTTPreq *req);
 
-int main() {
-    HTTP* serve = new_http("127.0.0.1:7545");
+int main(void) {
+    HTTP *serve = new_http("127.0.0.1:7545");
 
     handle_http(serve, "/", index_page);
-    handle_http(serve, "/about", about_page);
+    handle_http(serve, "/about/", about_page);
 
     listen_http(serve);
     free_http(serve);
+    return 0;
 }
 
-void index_page(int conn, HTTPreq* request) {
-    if (strcmp(request->path, "/") != 0) {
+void index_page(int conn, HTTPreq *req) {
+    if (strcmp(req->path, "/") != 0) {
         parsehtml_http(conn, "page404.html");
         return;
     }
     parsehtml_http(conn, "index.html");
 }
 
-void about_page(int conn, HTTPreq* request) {
-    if (strcmp(request->path, "/about") != 0) {
+void about_page(int conn, HTTPreq *req) {
+    if (strcmp(req->path, "/about/") != 0) {
         parsehtml_http(conn, "page404.html");
         return;
     }
